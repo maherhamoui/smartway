43662f647bbbc0faec6f8f42fab3fb13846af472
arma <arma@Aram-pc>
2014-08-10 23:01:47 +0300

    before pulling build.gradle

--- src/main/java/org/gradle/tools/FileXlReader.java
+++ src/main/java/org/gradle/tools/FileXlReader.java
@@ -36,7 +36,17 @@ public class FileXlReader {
 					while (cellIterator.hasNext()) {
 						
 						Cell cell = cellIterator.next();
-						
+						switch(cell.getCellType()) {
+		                case Cell.CELL_TYPE_BOOLEAN:
+		                    System.out.print(cell.getBooleanCellValue() + "\t\t");
+		                    break;
+		                case Cell.CELL_TYPE_NUMERIC:
+		                    System.out.print(cell.getNumericCellValue() + "\t\t");
+		                    break;
+		                case Cell.CELL_TYPE_STRING:
+		                    System.out.print(cell.getStringCellValue() + "\t\t");
+		                    break;
+		            }
 						
 					}
 				}
