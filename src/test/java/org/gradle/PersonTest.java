fd908f7c414db2f8ed4122ac220ec5af637a02a4
arma <arma@Aram-pc>
2014-08-10 11:03:31 +0300

    initial commit after working

--- src/test/java/org/gradle/PersonTest.java
+++ src/test/java/org/gradle/PersonTest.java
@@ -6,7 +6,6 @@ import static org.junit.Assert.*;
 public class PersonTest {
     @Test
     public void canConstructAPersonWithAName() {
-        Person person = new Person("Larry");
-        assertEquals("Larry", person.getName());
+       
     }
 }
